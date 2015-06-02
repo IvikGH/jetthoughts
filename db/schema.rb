fc30caf12e5edbc9d1276a6c5fc5bf449d27a2a2
Ivikgh <andoidxx@gmail.com>
2015-06-02 16:17:02 +0300

    Add Geo fields to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150529154857) do
+ActiveRecord::Schema.define(version: 20150602092507) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,14 @@ ActiveRecord::Schema.define(version: 20150529154857) do
     t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "login"
+    t.string   "full_name"
+    t.string   "birthday"
+    t.string   "address"
+    t.string   "city"
+    t.string   "state"
+    t.string   "country"
+    t.integer  "zip"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
